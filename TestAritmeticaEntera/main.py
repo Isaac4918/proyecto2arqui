b0091f84b39562214235377ee8dbcb5da9281a51
Ian Coto <113566538+iancotosoto@users.noreply.github.com>
2024-06-09 11:12:39 -0600

    Annade carpeta con AritmeticaEntera
    Se ejecuta la generación de la llave 1000 veces (puede ser modificado) y se toman tiempos de inicio y fin, luego se obtiene la duración de cada iteración y se saca el promedio.

---
+++ TestAritmeticaEntera/main.py
@@ -0,0 +1,9 @@
+from AritmeticaEntera import KeyManagement
+
+# Función principal para ejecutar el código y medir el tiempo
+def main():
+    KeyManagement.benchmark()
+
+# Ejecutar la función principal
+if __name__ == "__main__":
+    main()
